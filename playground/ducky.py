873a787ea9ca0798c761559efca0f117ca6ce62c
Andy Armstrong <andy@hexten.net>
2025-03-21 14:57:18 +0000

    Minor

--- playground/ducky.py
+++ playground/ducky.py
@@ -1,28 +1,35 @@
 import marimo
 
 __generated_with = "0.11.23"
-app = marimo.App(width="medium")
+app = marimo.App(width="columns")
+
+
+@app.cell(column=0)
+def _(Rule, Via, uniq):
+    via = Via(
+        Rule(
+            via=[
+                Via(
+                    Rule(src="$._id", dst="$.id"),
+                    Rule(src="$.ids[*].value", dst="$.ids[*]"),
+                    Rule(src="$..source", dst="$.sources[*]"),
+                    Rule(src="$..title.value", dst="$.titles[*]"),
+                    Rule(src="$..value", dst="$.values[*]"),
+                ),
+                Via(
+                    Rule(),
+                    Rule(src=["$.sources", "$.values"], map=uniq),
+                ),
+            ]
+        )
+    )
+    return (via,)
 
 
 @app.cell
-def _():
-    import json
-
-    import pycouchdb
-    return json, pycouchdb
-
-
-@app.cell
-def _():
-    import duckdb
-    from via_jsonpath import Rule, Via
-    return Rule, Via, duckdb
-
-
-@app.cell
-def _():
-    import marimo as mo
-    return (mo,)
+def _(db):
+    doc = db.get("match-id:0008fb2ddc6880c84d430feeb9195038")
+    return (doc,)
 
 
 @app.cell
@@ -33,26 +40,30 @@ def _(pycouchdb):
 
 
 @app.cell
-def _(Rule, Via):
-    via = Via(
-        Rule(src="$._id", dst="$.id"),
-        Rule(src="$.ids[*].value", dst="$.ids[*]"),
-        Rule(src="$..source", dst="$.sources[*]"),
-        Rule(src="$..title.value", dst="$.titles[*]"),
-    )
-    return (via,)
+def uniq():
+    def uniq(x: list[str]) -> list[str]:
+        return list(set(x))
+    return (uniq,)
 
 
 @app.cell
-def _(db):
-    doc = db.get("match-id:00034dfdb5f6ade2308057c539a86151")
-    return (doc,)
+def _():
+    import json
+    import pycouchdb
+    import marimo as mo
+    from via_jsonpath import Rule, Via
+    return Rule, Via, json, mo, pycouchdb
 
 
-@app.cell
-def _(doc, json, via):
-    cooked = via.transform(doc)
+@app.cell(column=1)
+def _(cooked, json):
     print(json.dumps(cooked, indent=2))
+    return
+
+
+@app.cell
+def _(doc, via):
+    cooked = via.transform(doc)
     return (cooked,)
 
 
