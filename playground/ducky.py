e4b64fbefb9e42102e80ea6ee79ac8e9bca46873
Andy Armstrong <andy@hexten.net>
2025-03-31 16:44:54 +0100

    Ducky

--- playground/ducky.py
+++ playground/ducky.py
@@ -4,32 +4,102 @@ __generated_with = "0.11.23"
 app = marimo.App(width="columns")
 
 
-@app.cell(column=0)
-def _(Rule, Via, uniq):
-    via = Via(
-        Rule(
-            via=[
-                Via(
-                    Rule(src="$._id", dst="$.id"),
-                    Rule(src="$.ids[*].value", dst="$.ids[*]"),
-                    Rule(src="$..source", dst="$.sources[*]"),
-                    Rule(src="$..title.value", dst="$.titles[*]"),
-                    Rule(src="$..value", dst="$.values[*]"),
-                ),
-                Via(
-                    Rule(),
-                    Rule(src=["$.sources", "$.values"], map=uniq),
-                ),
-            ]
-        )
+@app.cell(column=0, hide_code=True)
+def _(sample_slider):
+    sample_slider
+    return
+
+
+@app.cell
+def _(Rule, Via, date_time, lit, uniq):
+    via = Via.chain(
+        Via(
+            Rule(dst="$.txTimes", via=lit([])),
+            Rule(
+                src="$.transmissions[*]",
+                dst="$.txTimes[*]",
+                via=date_time("$.transmissionDate", "$.transmissionStartTime"),
+            ),
+            Rule(src="$..source", dst="$.sources[*]"),
+            Rule(
+                src=[
+                    "$._id",
+                    "$..ids[*].value",
+                    "$..id",
+                    "$.transmissions[*].transmissionId",
+                    "$.programmeNumber",
+                ],
+                dst="$.ids[*]",
+            ),
+        ),
+        Via(
+            Rule(),
+            Rule(
+                src=[
+                    "$.txTimes",
+                    "$.sources",
+                    "$.ids",
+                ],
+                map=[sorted,uniq],
+            ),
+        ),
     )
     return (via,)
 
 
 @app.cell
+def _(Deleted, JP, ViaContext):
+    def date_time(date_path: str, time_path: str):
+        date_path = JP(date_path)
+        time_path = JP(time_path)
+        def vf(ctx: ViaContext):
+            tx_time = ctx.get(time_path)
+            if tx_time == Deleted:
+                return ctx.get(date_path)
+            else:
+                return ctx.get(date_path) + "T" + tx_time
+
+        return vf
+    return (date_time,)
+
+
+@app.cell(column=1)
+def _(cooked, json):
+    print(json.dumps(cooked, indent=2))
+    return
+
+
+@app.cell
+def _(doc, via):
+    cooked = via.transform(doc)
+    return (cooked,)
+
+
+@app.cell
+def _(doc, json):
+    print(json.dumps(doc, indent=2))
+    return
+
+
+@app.cell(column=2)
+def _(db, sample_ids, sample_slider):
+    current_id = sample_ids[sample_slider.value]
+    doc = db.get(current_id)
+    return current_id, doc
+
+
+@app.cell
+def _(mo, sample_ids):
+    sample_slider = mo.ui.slider(start=0, stop=len(sample_ids)-1, step=1)
+    return (sample_slider,)
+
+
+@app.cell
 def _(db):
-    doc = db.get("match-id:0008fb2ddc6880c84d430feeb9195038")
-    return (doc,)
+    sample = db.query("sample/sample", limit=25, update="lazy", start_key=0.4)
+    sample_ids = sorted([row["id"] for row in sample])
+
+    return sample, sample_ids
 
 
 @app.cell
@@ -51,20 +121,8 @@ def _():
     import json
     import pycouchdb
     import marimo as mo
-    from via_jsonpath import Rule, Via
-    return Rule, Via, json, mo, pycouchdb
-
-
-@app.cell(column=1)
-def _(cooked, json):
-    print(json.dumps(cooked, indent=2))
-    return
-
-
-@app.cell
-def _(doc, via):
-    cooked = via.transform(doc)
-    return (cooked,)
+    from via_jsonpath import Rule, Via, ViaContext, JP, Deleted, lit
+    return Deleted, JP, Rule, Via, ViaContext, json, lit, mo, pycouchdb
 
 
 if __name__ == "__main__":
